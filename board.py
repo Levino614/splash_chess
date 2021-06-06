75962e6ad393b40bf43b8c1978906c9e7468fcf7
Akatos <a.dehning99@web.de>
2021-06-06 19:13:57 +0200

    Coole Namen

--- board.py
+++ board.py
@@ -1,15 +1,15 @@
-def CreateBoard():
-    chessBoard = []
+def create_board():
+    chess_board = []
     for i in range(8):
         rank = []
         for j in range(8):
-            isDarksquare = (i + j) % 2 != 0
-            rank.append(isDarksquare)
-        chessBoard.append(rank)
-    return chessBoard
+            is_dark_square = (i + j) % 2 != 0
+            rank.append(is_dark_square)
+        chess_board.append(rank)
+    return chess_board
 
 
-board = CreateBoard()
+board = create_board()
 
 for row in board:
     print(row)
