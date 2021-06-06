a0d21120b138ef715cb28a2dc25cdc9b50cb1de5
Akatos <a.dehning99@web.de>
2021-06-06 19:25:41 +0200

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,33 +1,39 @@
 class Piece:
-    rank_pos = 0
-    file_pos = 0
+    is_white = False
+    rank = 0
+    file = 0
+
+    def __init__(self, is_white, rank, file):
+        self.is_white = is_white
+        self.rank = rank
+        self.file = file
 
 
 class Knight(Piece):
-    def getMoves(self):
+    def get_moves(self):
         pass
 
 
 class Rook(Piece):
-    def getMoves(self):
+    def get_moves(self):
         pass
 
 
 class Bishop(Piece):
-    def getMoves(self):
+    def get_moves(self):
         pass
 
 
 class Queen(Piece):
-    def getMoves(self):
+    def get_moves(self):
         pass
 
 
 class King(Piece):
-    def getMoves(self):
+    def get_moves(self):
         pass
 
 
 class Pawn(Piece):
-    def getMoves(self):
+    def get_moves(self):
         pass
